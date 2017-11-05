8566d44e0621578413a9e7f975bb34ec001c6712
SentidosDigitales <sentidos.digitales@gmail.com>
2017-11-05 18:04:08 -0300

    tp final

--- public_html/index.js
+++ public_html/index.js
@@ -5,3 +5,61 @@
  */
 
 
+var peliculas = new Array();
+
+function addPelicula(pelicula) {
+    for (var i = 0; i < peliculas.length; i++) {
+        if (pelicula.titulo == peliculas[i].titulo) {
+            console.log("Pelicula ya ingresada");
+            return "repetida";
+        }
+    }
+    peliculas.push(pelicula);
+}
+
+function orderPeliculas() {
+    peliculas.sort();
+}
+
+function deleteByID(id) {
+    for (var i = 0; i < peliculas.length; i++) {
+        if (peliculas[i].ID == id) {
+            peliculas.splice(i, i + 1);
+            break;
+        }
+    }
+}
+function Pelicula(id, titulo) {
+    this.ID = id;
+    this.titulo = titulo;
+}
+
+var peli1 = new Pelicula("1", "El secreto de sus ojos");
+var peli2 = new Pelicula("3", "Día de la independencia");
+var peli3 = new Pelicula("2", "JUMAJI");
+
+peliculas = [peli1, peli2, peli3];
+
+function orderById() {
+    peliculas.sort(function (a, b) {
+        if (a.ID > b.ID) {
+            return 1;
+        }
+        if (a.ID < b.ID) {
+            return -1;
+        }
+        return 0;
+    });
+}
+
+function orderByTitulo() {
+    peliculas.sort(function (a, b) {
+        if (a.titulo > b.titulo) {
+            return 1;
+        }
+        if (a.titulo < b.titulo) {
+            return -1;
+        }
+        return 0;
+    });
+}
